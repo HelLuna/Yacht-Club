a436accd44f89fe6861a67682b12b24cdde0329e
ChocoLutter <73901812+HelLuna@users.noreply.github.com>
2024-09-19 23:33:29 +0300

    Corrected change-image.js

--- js/change-image.js
+++ js/change-image.js
@@ -2,14 +2,14 @@
 let activePhoto = document.querySelector('.active-img');
 let previews = document.querySelectorAll('.gallery-preview a');
 
-for (let preivew of previews) {
-  preivew.addEventListener('click', function(evt) {
+for (let preview of previews) {
+  preview.addEventListener('click', function(evt) {
     evt.preventDefault();
 
     let currentActive = document.querySelector('.gallery-preview .active-item');
     currentActive.classList.remove('active-item');
-    preivew.classList.add('active-item');
+    preview.classList.add('active-item');
 
-    activePhoto.src = preivew.href;
+    activePhoto.src = preview.href;
   });
 }
